859b8ff3242db1178395fc9c89d343161dd82586
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-14 17:56:22 +0900

    수정추가중

--- src/main/java/com/tenco/blog/user/User.java
+++ src/main/java/com/tenco/blog/user/User.java
@@ -39,4 +39,16 @@ public class User {
         this.email = email;
         this.createdAt = createdAt;
     }
+
+    public void Update(UserRequest.UpdateDTO updateDTO) {
+        updateDTO.validate();
+
+        // 영속 상태 엔티티의 필드값을 변경
+        this.password = updateDTO.getPassword();
+        this.email = updateDTO.getEmail();
+
+        // 변경 감시(Dirty checking)
+        // 1. 영속성 컨텍스트가 최초 상태를 스냅샷 보관
+
+    }
 }
---
+++ src/main/java/com/tenco/blog/user/UserResponse.java
@@ -0,0 +1,55 @@
+package com.tenco.blog.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+public class UserResponse {
+
+    // 회원 가입 후 응답 DTO
+    @Data
+    public static class JoinDTO{
+        private Long id;
+        private String username;
+        private String email;
+        private String createdAt;
+
+        // 서버 단에서 new UserResponse.JoinDTO(..)
+        @Builder
+        public JoinDTO(User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.email = user.getEmail();
+            this.createdAt = user.getCreatedAt().toString();
+        }
+    }
+
+    // 로그인 후 응답 DTO
+    @Data
+    public static class LoginDTO{
+        private Long id;
+        private String username;
+        private String email;
+
+        @Builder
+        public LoginDTO(User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.email =user.getEmail();
+        }
+    }
+
+    // 사용자 정보 조회 응답 DTO
+    @Data
+    public static class DetailDTO{
+        private Long id;
+        private String username;
+        private String email;
+
+        @Builder
+        public DetailDTO(User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.email = user.getEmail();
+        }
+    }
+}
